5cf5e19bd59976c73f7540a5cd32fca7bb8105e0
JohanCarlsen <johan_carlsen@hotmail.com>
2023-09-22 23:55:18 +0200

    New layout

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,8 +15,15 @@ author = 'Johan Carlsen'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
-
+extensions = [
+    'sphinx.ext.autodoc', 
+    'sphinx.ext.napoleon', 
+    'numpydoc', 
+    'sphinx.ext.autosummary',
+    'sphinx_autodoc_typehints'
+]
+autosummary_generate = True
+autosummary_imported_members = True
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -25,6 +32,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-# html_theme = 'pydata_sphinx_theme'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
